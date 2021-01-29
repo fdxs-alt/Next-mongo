38a2e2b550b087786497d4b89cf708274e4dc867
Jakub Sukiennik <kuba1207710@gmail.com>
2021-01-29 09:38:30 +0100

    reset project

---
+++ src/pages/api/index.ts
@@ -0,0 +1,5 @@
+import { NextApiResponse } from "next";
+import { NextApiRequest } from "next";
+export default function (req: NextApiRequest, res: NextApiResponse) {
+  res.json({ message: "message" });
+}
